db02ed6cf64370273f0660a8d081140916be2356
rajashekarreddypr24@gmail.com <rajashekarreddypr24@gmail.com>
2023-03-07 04:26:16 +0000

    07/03/2023

--- models/Test.sql
+++ models/Test.sql
@@ -1,9 +1,19 @@
 
 
-select * from test._airbyte_raw_sheet1
+-- select * from test._airbyte_raw_sheet1
 
-select * from test.sheet1
+-- select * from test.sheet1
 
-alter test.sheet1 rename column city to city1
+-- alter test.sheet1 rename column city to city1
 
-update sheet1 set city = city1
+-- update sheet1 set city = city1
+
+
+-- Rename a column in my_table
+{{  rename_column(
+      schema = 'test',
+      table = 'sheet1',
+      old_column_name = 'city',
+      new_column_name = 'city1'
+   ) 
+}}
